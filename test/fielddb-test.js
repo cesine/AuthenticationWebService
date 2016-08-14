ca04c56caad1167ceb1ad64bd52832aa19f68865
cesine <cesine@yahoo.com>
2016-08-14 14:34:39 -0400

    showing how to run fielddb tests

--- test/fielddb-test.js
+++ test/fielddb-test.js
@@ -1,5 +1,4 @@
-var basePath  = require("fielddb/tests/All-spec").basePath;
-basePath = "fielddb/tests/";
-
 var run = require("fielddb/tests/All-spec").runTests;
+
+var basePath = "fielddb/tests/";
 run();
